01b486e3fa58004a925aa532bea9bb0bff9ad059
DESKTOP-VS26VVU\vijay <vijayaratnamythanushan000@gmail.com>
2025-01-15 16:09:41 +0530

    changes stage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,9 @@
 module.exports = {
-  content: ['./pages/**/*.html', './components/**/*.html'],
+  content: ['./pages/**/*.html', './components/**/*.html', './index.html'],
   theme: {
     extend: {
-      colors: {
-        primary: '#1a202c',
-        secondary: '#2d3748',
-      },
-      fontFamily: {
-        custom: ['TT Supermolot Neue', 'sans-serif'], // Add your custom font
-      },
+      
     },
   },
   plugins: [],
 };
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
